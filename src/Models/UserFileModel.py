b5d93fa21404a556f4d62595bb142839c77e6332
mahmoudibrahims <mahmoudibrahimabdelfattah48@gmail.com>
2025-07-03 14:31:37 +0300

    create file User file

---
+++ src/Models/UserFileModel.py
@@ -0,0 +1,73 @@
+from .BaseDataModel import BaseDataModel
+from .schema.DBSchemas import UserFile
+from sqlalchemy.future import select
+from sqlalchemy import func
+
+
+class UserFileModel(BaseDataModel):
+    
+    def __init__(self, db_client: object):
+        super().__init__(db_client=db_client)
+        self.db_client = db_client
+
+    @classmethod
+    async def create_instance(cls, db_client: object):
+        return cls(db_client)
+
+    async def create_user_file(self, user_file: UserFile):
+        async with self.db_client() as session:
+            async with session.begin():
+                session.add(user_file)
+            await session.commit()
+            await session.refresh(user_file)
+        return user_file
+
+    async def get_file_or_create_one(
+        self,
+        file_uuid,
+        original_filename,
+        file_type,
+        file_size,
+        file_path,
+        project_id,
+        full_data,
+        storage_method="database"
+    ):
+        async with self.db_client() as session:
+            async with session.begin():
+                query = select(UserFile).where(UserFile.file_uuid == file_uuid)
+                result = await session.execute(query)
+                user_file = result.scalar_one_or_none()
+
+                if user_file is None:
+                    new_file = UserFile(
+                        file_uuid=file_uuid,
+                        original_filename=original_filename,
+                        file_type=file_type,
+                        file_size=file_size,
+                        file_path=file_path,
+                        project_id=project_id,
+                        full_data=full_data,
+                        storage_method=storage_method,
+                    )
+                    user_file = await self.create_user_file(new_file)
+
+                return user_file
+
+    async def get_all_files(self, page: int = 1, page_size: int = 10):
+        async with self.db_client() as session:
+            async with session.begin():
+                count_query = await session.execute(
+                    select(func.count(UserFile.file_id))
+                )
+                total_documents = count_query.scalar_one()
+
+                total_pages = total_documents // page_size
+                if total_documents % page_size > 0:
+                    total_pages += 1
+
+                query = select(UserFile).offset((page - 1) * page_size).limit(page_size)
+                result = await session.execute(query)
+                files = result.scalars().all()
+
+                return files, total_pages
