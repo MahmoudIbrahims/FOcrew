d33ab1f813081da0b6c888b3de80fcf652fd7509
mahmoudibrahims <mahmoudibrahimabdelfattah48@gmail.com>
2025-07-03 14:32:24 +0300

    Create Constant in this file

---
+++ src/Models/enums/DataBaseEnum.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class DataBaseEnum(Enum):
+    COLLECTION_PROJECT_NAME="projects"
+    COLLECTION_USER_FILES="user_files"
+    COLLECTION_AGENT_RESULT="agent_results"
+    COLLECTION_FILE_AGENT_RELATIONS="file_agent_relations"
+    
