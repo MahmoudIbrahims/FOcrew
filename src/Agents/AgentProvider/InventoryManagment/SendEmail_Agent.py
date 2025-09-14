7ca80947a2577680800c202ef8b27199085a0be6
mahmoudibrahims <mahmoudibrahimabdelfattah48@gmail.com>
2025-09-14 21:06:02 +0300

    Update Agent

--- src/Agents/AgentProvider/InventoryManagment/SendEmail_Agent.py
+++ src/Agents/AgentProvider/InventoryManagment/SendEmail_Agent.py
@@ -1,36 +1,36 @@
 from crewai import Task
 from ..BaseAgent import BaseAgent
 from Providers import ProviderLLM
-from tools.SendEmail_tool import WebhookTool
+from tools.SendEmail_tool import SendEmailTool
 
 class ReportSenderAgent(BaseAgent):
     def __init__(self):
         provider = ProviderLLM()
         llm = provider.get_llm()
-        zapier_tool = WebhookTool()
+        Gmail_tool= SendEmailTool()
 
         super().__init__(
-            name="Zapier Report Sender",
+            name="Gmail Report Sender",
             role="Email Dispatcher",
-            goal="Send generated reports to managers via Gmail (through Zapier Webhook)",
+            goal="Send generated reports to managers via Gmail",
             backstory="\n".join([
                 "You are responsible for delivering analysis results to stakeholders.",
-                "You send the final reports automatically to managers using Gmail through Zapier integration."
+                "You send the final reports automatically to managers using Gmail SMTP."
             ]),
             llm=llm,
             allow_delegation=False,
-            tools=[zapier_tool],
+            tools=[Gmail_tool],
         )
 
     def get_task(self):
         return Task(
             description="\n".join([
-                "Take the pdf analysis file (default: results/inventory_management/report.pdf).",
-                "1. Use the `Send Report to Zapier` tool.",
+                "Take the pdf analysis this file path (default: results/inventory_management/report.pdf).",
+                "1. Use the `Send Report to Gmail_tool`tool`.",
                 "2. Pass a suitable subject (like 'Inventory Report').",
                 "3. Pass the report body pdf or text or summary.",
-                "This will trigger Zapier → Gmail and send it to managers automatically."
+                "this is Gmail and send it to managers automatically."
             ]),
             agent=self.get_agent(),
-            expected_output="Report successfully sent to Zapier → Gmail"
+            expected_output="Report successfully sent → Gmail"
         )
